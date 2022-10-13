e38907d1d33436b26e52c7ca80a14d01605818ff
Josh <joshGdovindesigns.com>
2022-10-13 10:33:04 -0400

    working on demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,7 @@
-// console.log("hello class");
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 // Sets
 gsap.set("#hero h1 span",{alpha:0.25})
@@ -21,15 +23,31 @@ trailsBtn.addEventListener("mouseout",function(){
     heroButtonTL.reverse();
 })
 
-function heroAnimation(){
+// function heroAnimation(){
+//     var tl = gsap.timeline();
+//     tl.from("#first-line",{duration:1,alpha:0, y:-100})
+//     .from("#second-line",{duration:1,alpha:0, y:-100},"-=0.75")
+//     .from("#trails-btn",{duration:1,y:100, alpha:0},"-=.5")
+//     .from("#trails-btn i",{duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom"},"-=0.5");
+//     return tl;
+// }
+
+
+function boxAnimation(){
     var tl = gsap.timeline();
-    tl.from("#first-line",{duration:1,alpha:0, y:-100})
-    .from("#second-line",{duration:1,alpha:0, y:-100},"-=0.75")
-    .from("#trails-btn",{duration:1,y:100, alpha:0},"-=.5")
-    .from("#trails-btn i",{duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom"},"-=0.5");
+    tl.from("#box", {
+		duration: 1,
+		alpha: 0,
+		rotation: -180,
+		x: "-300%",
+		scrollTrigger: { trigger: "#box", markers: true, scrub:true, end:"top center", start:"top 80%", pin:true }
+	})
     return tl;
 }
 
 
+
 var mainTimeline = gsap.timeline();
-mainTimeline.add(heroAnimation());
+// mainTimeline.add(heroAnimation())
+
+mainTimeline.add(boxAnimation());
