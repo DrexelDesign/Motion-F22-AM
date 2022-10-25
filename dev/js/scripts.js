0e84ae83a0fd9ebb7952d2601028891f19140916
Josh <joshGdovindesigns.com>
2022-10-25 11:12:39 -0400

    demo complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,27 @@
 import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
-gsap.to(".square",{y:600, duration:2, fill:"red", strokeWidth:10,stagger:0.5});
+// gsap.set("#square",{rotation:180});
+
+
+
+function drawLines(){
+    var tl = gsap.timeline();
+    tl.from("#square",{duration:2, fill:"white", drawSVG:"50px 20px", rotate:180, transformOrigin:"center top"},"same")
+    .from("#line",{duration:2, drawSVG:"0%"},"same");
+    return tl;
+}
+
+var mainTl = gsap.timeline();
+mainTl.add(drawLines());
+
+
+
+
+
+
+GSDevTools.create();
